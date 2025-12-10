325d0b4be14ae9735b35784b4d02862635038d67
Duy Khang <khoakhang2017a@gmail.com>
2025-12-10 22:02:15 +0700

    Test

--- no-poverty/src/App.jsx
+++ no-poverty/src/App.jsx
@@ -1,39 +1,65 @@
-import { useEffect, useState } from 'react';
-import Navbar from './Components/Navbar/Navbar.jsx'
-import Home from './Pages/Home.jsx'
-import Video from './Pages/Video.jsx'
-import About from './Pages/About.jsx'
-import Footer from './Components/Footer/Footer.jsx'
-import AboutQuynh from './Pages/AboutQuynh.jsx'
-import AboutAnh from './Pages/AboutAnh.jsx'
-import AboutLoi from './Pages/AboutLoi.jsx'
-import AboutNghia from "./Pages/AboutNghia.jsx"
-import AboutKhang from './Pages/AboutKhang.jsx'
-import AboutKhoa from './Pages/AboutKhoa.jsx'
-import Contract from './Pages/Contract.jsx'
+import Navbar from './Components/Navbar/Navbar.jsx';
+import Home from './Pages/Home.jsx';
+import Video from './Pages/Video.jsx';
+import About from './Pages/About.jsx';
+import Footer from './Components/Footer/Footer.jsx';
 
-function App(){
-  const [route, setRoute] = useState(() => window.location.hash.replace('#','') || '/');
+import AboutQuynh from './Pages/AboutQuynh.jsx';
+import AboutAnh from './Pages/AboutAnh.jsx';
+import AboutLoi from './Pages/AboutLoi.jsx';
+import AboutNghia from './Pages/AboutNghia.jsx';
+import AboutKhang from './Pages/AboutKhang.jsx';
+import AboutKhoa from './Pages/AboutKhoa.jsx';
 
-  useEffect(() => {
-    const onHash = () => setRoute(window.location.hash.replace('#','') || '/');
-    window.addEventListener('hashchange', onHash);
-    return () => window.removeEventListener('hashchange', onHash);
-  }, []);
+import Contract from './Pages/Contract.jsx';
 
-  let Component = null;
-  switch(route){
-    case "/": Component = <Home />; break;
-    case "/video": Component = <Video />; break;
-    case "/about": Component = <About />; break;
-    case "/contract": Component = <Contract />; break;
-    case "/quynh": Component = <AboutQuynh />; break;
-    case "/anh": Component = <AboutAnh />; break;
-    case "/loi": Component = <AboutLoi />; break;
-    case "/nghia": Component = <AboutNghia />; break;
-    case "/khang": Component = <AboutKhang />; break;
-    case "/khoa": Component = <AboutKhoa />; break;
-    default: Component = <Home />; break;
+function App() {
+  let Component;
+
+  switch (window.location.pathname) {
+    case "/no-poverty/":
+      Component = <Home />;
+      break;
+
+    case "/no-poverty/video":
+      Component = <Video />;
+      break;
+
+    case "/no-poverty/about":
+      Component = <About />;
+      break;
+
+    case "/no-poverty/contract":
+      Component = <Contract />;
+      break;
+
+    case "/no-poverty/quynh":
+      Component = <AboutQuynh />;
+      break;
+
+    case "/no-poverty/anh":
+      Component = <AboutAnh />;
+      break;
+
+    case "/no-poverty/loi":
+      Component = <AboutLoi />;
+      break;
+
+    case "/no-poverty/nghia":
+      Component = <AboutNghia />;
+      break;
+
+    case "/no-poverty/khang":
+      Component = <AboutKhang />;
+      break;
+
+    case "/no-poverty/khoa":
+      Component = <AboutKhoa />;
+      break;
+
+    default:
+      Component = <Home />;
+      break;
   }
 
   return (
@@ -44,4 +70,5 @@ function App(){
     </>
   );
 }
+
 export default App;
